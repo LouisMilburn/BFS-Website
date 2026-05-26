9ee3ffa8b57aa62b885d8a04067cd01245923934
LouisMilburn <louismilburn@icloud.com>
2026-05-26 11:57:57 +0100

    Add SEO (OG tags, Schema.org, sitemap, robots.txt), FAQ page, 404 page, testimonials placeholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://bfswebsite.louismilburn.workers.dev',
   integrations: [tailwind()],
   output: 'static',
 });
